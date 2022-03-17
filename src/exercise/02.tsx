305ac441dea094e8ab2e4f06bdfc4446fe2d21a2
Matthew Morgan <mattmorgan@khanacademy.org>
2022-03-17 09:23:52 -0400

    Complete exercise 2 -- not extra credit yet

--- src/exercise/02.tsx
+++ src/exercise/02.tsx
@@ -10,74 +10,90 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
-// 🐨 this is going to be our generic asyncReducer
-function pokemonInfoReducer(state, action) {
+interface PokemonData {
+  name: string,
+  image: string,
+}
+
+type State = {
+  status: "idle",
+} | {
+  status: "pending",
+} | {
+  status: "resolved",
+  data: PokemonData,
+} | {
+  status: "rejected",
+  error: {message: string},
+};
+
+type Action = {
+  type: "pending",
+} | {
+  type: "resolved",
+  data: PokemonData
+} | {
+  type: "rejected",
+  error: {message: string},
+};
+
+function asyncReducer(state: State, action: Action): State {
   switch (action.type) {
     case 'pending': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'pending', pokemon: null, error: null}
+      return {status: 'pending'}
     }
     case 'resolved': {
-      // 🐨 replace "pokemon" with "data" (in the action too!)
-      return {status: 'resolved', pokemon: action.pokemon, error: null}
+      return {status: 'resolved', data: action.data}
     }
     case 'rejected': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'rejected', pokemon: null, error: action.error}
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
+      return {status: 'rejected', error: action.error}
     }
   }
 }
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
-  // here's how you use it:
-  // const state = useAsync(
-  //   () => {
-  //     if (!pokemonName) {
-  //       return
-  //     }
-  //     return fetchPokemon(pokemonName)
-  //   },
-  //   {status: pokemonName ? 'pending' : 'idle'},
-  //   [pokemonName],
-  // )
-  // 🐨 so your job is to create a useAsync function that makes this work.
-  const [state, dispatch] = React.useReducer(pokemonInfoReducer, {
-    status: pokemonName ? 'pending' : 'idle',
-    // 🐨 this will need to be "data" instead of "pokemon"
-    pokemon: null,
-    error: null,
+function useAsync(asyncCallback: () => Promise<State>, initialState: State, dependencies: [string]) {
+  const [state, dispatch] = React.useReducer(asyncReducer, {
+    status: 'idle',
+    ...initialState,
   })
 
   React.useEffect(() => {
-    // 💰 this first early-exit bit is a little tricky, so let me give you a hint:
-    // const promise = asyncCallback()
-    // if (!promise) {
-    //   return
-    // }
-    // then you can dispatch and handle the promise etc...
-    if (!pokemonName) {
+    const promise = asyncCallback()
+    if (!promise) {
       return
     }
     dispatch({type: 'pending'})
-    fetchPokemon(pokemonName).then(
-      pokemon => {
-        dispatch({type: 'resolved', pokemon})
-      },
-      error => {
-        dispatch({type: 'rejected', error})
-      },
+    promise.then(
+        (data) => {
+          if (data.status !== "resolved"){
+            throw new Error("Impossible state");
+          }
+          // @ts-ignore
+          dispatch({type: 'resolved', data})
+        },
+        error => {
+          dispatch({type: 'rejected', error})
+        },
     )
-    // 🐨 you'll accept dependencies as an array and pass that here.
-    // 🐨 because of limitations with ESLint, you'll need to ignore
-    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
-  }, [pokemonName])
+  }, dependencies)
 
-  // 🐨 this will change from "pokemon" to "data"
-  const {pokemon, status, error} = state
+  return state
+}
+
+function PokemonInfo({pokemonName}) {
+  const state = useAsync(
+      () => {
+        if (!pokemonName) {
+          return
+        }
+        return fetchPokemon(pokemonName)
+      },
+      {status: pokemonName ? 'pending' : 'idle'},
+      [pokemonName],
+  )
+
+  // @ts-ignore
+  const {data: pokemon, status, error} = state
 
   if (status === 'idle' || !pokemonName) {
     return <div>Submit a pokemon</div>
